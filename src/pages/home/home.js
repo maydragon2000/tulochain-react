2fb39e4e43770e31375bdae9c93917ada6daaf87
maydragon2000 <may-dragon2000@outlook.com>
2022-04-10 20:49:54 -0700

    basic structure

---
+++ src/pages/home/home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "./header";
+const Home = () => {
+    return (<div className="hfeed site" id="page">
+        <Header />
+
+    </div>)
+}
+export default Home;
